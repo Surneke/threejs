6fc92f7b036bd6c37280e3d7d6d0bcf816607324
Doljinsuren <doljinsuren.t@techpack.mn>
2023-09-25 12:45:16 +0800

    ouu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import { Suspense } from "react";
+import { Model } from "../public/blender/model";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Canvas camera={{ position: [5, 0, 0] }}>
+        <mesh>
+          <Suspense>{/* <Model /> */}</Suspense>
+        </mesh>
+      </Canvas>
     </div>
   );
 }
