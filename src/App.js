496c0735a9aeb05cb8cf3efb9d5daec840e08060
Doljinsuren <doljinsuren.t@techpack.mn>
2023-09-25 12:52:38 +0800

    ho

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { Suspense } from "react";
-import { Model } from "../public/blender/model";
+import { Model } from "./assets/model.js";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Canvas camera={{ position: [5, 0, 0] }}>
         <mesh>
-          <Suspense>{/* <Model /> */}</Suspense>
+          <Suspense>
+            <Model />
+          </Suspense>
         </mesh>
       </Canvas>
     </div>
