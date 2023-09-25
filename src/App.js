b39a49f5f000f78ea18fca94944eab213ce683f5
Doljinsuren <doljinsuren.t@techpack.mn>
2023-09-25 13:00:58 +0800

    jk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { Suspense } from "react";
-import { Model } from "./assets/model.js";
+// import { Model } from "./assets/model.js";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
       <Canvas camera={{ position: [5, 0, 0] }}>
-        <mesh>
-          <Suspense>
-            <Model />
-          </Suspense>
+        <mesh scale={0.5}>
+          <Suspense>{/* <Model /> */}</Suspense>
         </mesh>
+        <OrbitControls
+          enableZoom={false}
+          minPolarAngle={1.45}
+          maxPolarAngle={Math.PI / 1.85}
+          autoRotate
+          enableRotate={true}
+          autoRotateSpeed={5}
+          rotateSpeed={0.5}
+        />
       </Canvas>
     </div>
   );
